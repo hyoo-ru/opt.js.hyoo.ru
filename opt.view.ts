ab09f4f6d94cb0b15580f457a5cd90eb0e99677a
jin <jin@hyoo.ru>
2022-05-30 20:59:36 +0300

    Filter by type

--- opt.view.ts
+++ opt.view.ts
@@ -136,12 +136,24 @@ namespace $.$$ {
 		
 		@ $mol_mem
 		filters() {
+			const points = this.points()
 			const next = {} as Record< string, string >
+			
+			if( points.some( point => point.type === 'Fun' ) ) {
+				next[ 'Opt' ] = 'Opt'
+			}
+			
+			if( points.some( point => point.type === 'InlinedFun' ) ) {
+				next[ 'Inlined' ] = 'Inlined'
+			}
+			
 			for( const point of this.points() ) {
 				for( const reason of ( point.reasons ?? [] ) ) {
+					if( !reason ) continue
 					next[ reason ] = reason
 				}
 			}
+			
 			return next
 		}
 		
@@ -161,8 +173,8 @@ namespace $.$$ {
 			const points = this.points()
 			return this.points_followers().filter( (_, index ) => {
 				const point = points[ index ]
-				if( !point.reasons ) return true
-				if( !point.reasons.length ) return true
+				if( point.type === 'Fun' ) return this.filter_enabled( 'Opt' )
+				if( point.type === 'InlinedFun' ) return this.filter_enabled( 'Inlined' )
 				return point.reasons.some( reason => this.filter_enabled( reason ) )
 			} )
 		}
