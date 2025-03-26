1b2564c3ff80662b0df44b22ce72c1bbae5be748
nin-jin <nin-jin@users.noreply.github.com>
2025-03-26 07:22:44 +0000

    Deploying to gh-pages from @ hyoo-ru/opt.js.hyoo.ru@fe126ad4ba127d850bfd839e5eb49ef8a4c55235 🚀

--- web.d.ts
+++ web.d.ts
@@ -2740,6 +2740,7 @@ declare namespace $.$$ {
         load(frame: HTMLIFrameElement): Promise<Window>;
         uri_resource(): string;
         message_listener(): $mol_dom_listener;
+        sub(): readonly any[];
         message_receive(event?: MessageEvent<[string, string]>): void;
         uri_change(event: MessageEvent<[string, string]>): void;
         auto(): (Window | $mol_dom_listener)[];
