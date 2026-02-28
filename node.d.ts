5f916c17e650df5a1c0d619080a526a038fb2468
nin-jin <nin-jin@users.noreply.github.com>
2026-02-28 07:33:39 +0000

    Deploying to gh-pages from @ hyoo-ru/opt.js.hyoo.ru@fe126ad4ba127d850bfd839e5eb49ef8a4c55235 🚀

--- node.d.ts
+++ node.d.ts
@@ -1140,7 +1140,7 @@ declare namespace $ {
 }
 
 declare namespace $ {
-    let $mol_gap: Record<"text" | "block" | "space" | "blur" | "page" | "round", $mol_style_func<"var", unknown>>;
+    let $mol_gap: Record<"text" | "block" | "space" | "blur" | "page" | "round" | "emoji", $mol_style_func<"var", unknown>>;
 }
 
 declare namespace $ {
