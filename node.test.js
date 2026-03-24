a777a1702dea27032c17cdc9b13fcc67ef783a1c
nin-jin <nin-jin@users.noreply.github.com>
2026-03-24 07:57:49 +0000

    Deploying to gh-pages from @ hyoo-ru/opt.js.hyoo.ru@fe126ad4ba127d850bfd839e5eb49ef8a4c55235 🚀

--- node.test.js
+++ node.test.js
@@ -6710,6 +6710,7 @@ var $;
 "use strict";
 var $;
 (function ($) {
+    let x = /x/[Symbol.matchAll];
     class $mol_regexp extends RegExp {
         groups;
         constructor(source, flags = 'gsu', groups = []) {
